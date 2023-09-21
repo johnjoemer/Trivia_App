02663e4e5a1164e57c92a649cc8d932af8dd7cf4
johnjoemer <johnjoemerguevarra@gmail.com>
2023-09-21 15:27:37 +0800

    app can now store data.
    it can now store the player's name and their score and display who has the highest score among all the players

--- trivia_quiz_app/lib/main.dart
+++ trivia_quiz_app/lib/main.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:trivia_quiz_app/screens/home_page.dart';
+import 'package:trivia_quiz_app/services/database_helper.dart';
 
 String dispName = "";
 bool showText = false;
 
 void main() async {
   runApp(const MyApp());
+
+  final dbHelper = DatabaseHelper();
+  final allScores = await dbHelper.getAllScores();
+
+  for (final score in allScores){
+    final playerName = score['playerName'];
+    final playerScore = score['score'];
+    print('Player Name: $playerName, Score: $playerScore');
+  }
+  
 }
 
 class MyApp extends StatelessWidget {
@@ -51,8 +62,8 @@ class _MyHomePageState extends State<MyHomePage> {
       child: Column(
          mainAxisAlignment: MainAxisAlignment.center,
          children: <Widget>[
-          if(showText)
-          Text('Highest Score: $dispName = $highScore'),
+          // if(showText)
+          // Text('Highest Score: $dispName = $highScore'),
           const Text("Enter your name to proceed"),
 
           const SizedBox(height: 15),
@@ -97,6 +108,57 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     )
           
+    );
+  }
+}
+
+class MyHomePageWithScore extends StatelessWidget {
+  final String playerName;
+  final int highestScore;
+
+  MyHomePageWithScore({
+    required this.playerName,
+    required this.highestScore,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Trivia Quiz App'),
+      ),
+      body: Center (
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Highest Score is: $highestScore by $playerName',
+              style: const TextStyle(fontSize: 18),
+            ),
+            
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color.fromARGB(255, 30, 181, 100),
+              ),
+              onPressed: () async {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const MyHomePage(
+                    ),
+                  ),
+                );
+              },
+              child: const Text(
+                'Play Again',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- trivia_quiz_app/lib/services/database_helper.dart
+++ trivia_quiz_app/lib/services/database_helper.dart
@@ -1,29 +1,68 @@
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
-import 'package:trivia_quiz_app/resources/username_model.dart';
+// import 'package:trivia_quiz_app/resources/username_model.dart';
 
 class DatabaseHelper{
-  static const int _version = 1;
-  static const String _dbname = "Scores.db";
+  static final DatabaseHelper _instance = DatabaseHelper._internal();
+  factory DatabaseHelper() => _instance;
+  static Database? _db;
 
-  static Future<Database> _getDB() async {
-    return openDatabase(join (await getDatabasesPath(), _dbname),
-      onCreate: (db, version) async => await db.execute(
-        "CREATE TABLE Scores(username TEXT NOT NULL, score INTEGER);"),
-        version: _version);
+  DatabaseHelper._internal();
+
+  // Initialize the database
+  Future<Database?> get db async {
+    if(_db != null) return _db;
+    _db = await initDb();
+    return _db;
   }
 
-  static Future<int> addUser(userName username) async {
-    final db = await _getDB();
-    return await db.insert("username", username.toJson(),
-          conflictAlgorithm: ConflictAlgorithm.replace);
+  // Create and initialize the database
+  Future<Database> initDb() async {
+    final String databasesPath = await getDatabasesPath();
+    final String path = join(databasesPath, 'quiz_scores.db');
+
+    return await openDatabase(path, version: 1, onCreate: (Database db, int version) async {
+      await db.execute('''
+        CREATE TABLE scores (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          playerName TEXT,
+          score INTEGER
+        )
+        ''');
+    });
   }
 
-  static Future<int> updateUser(userName username) async {
-    final db = await _getDB();
-    return await db.update("username", username.toJson(),
-          where: 'username = ?',
-          whereArgs: [username],
-          conflictAlgorithm: ConflictAlgorithm.replace);
+  // Insert or update a player's score
+  Future<void> insertOrUpdateScore(String playerName, int score) async {
+    final dbClient = await db;
+    await dbClient!.insert(
+      'scores',
+      {'playerName': playerName, 'score': score},
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
   }
-}
+
+  // Get the highest score for a player 
+  Future<Map<String, dynamic>?> getHighestScore() async {
+    final dbClient = await db;
+    final List<Map<String, dynamic>> result = await dbClient!.rawQuery('''
+      SELECT playerName, MAX(score) AS highestScore
+      FROM scores
+    ''');
+
+    if (result.isNotEmpty) {
+      final playerName = result.first['playerName'];
+      final highestScore = result.first['highestScore'] as int;
+      return {'playerName': playerName, 'highestScore': highestScore};
+    }
+    else{
+      return null;
+    }
+  }
+
+  Future<List<Map<String, dynamic>>> getAllScores() async {
+    final dbClient = await db;
+    final List<Map<String, dynamic>> scores = await dbClient!.query('scores');
+    return scores;
+  }
+  }
